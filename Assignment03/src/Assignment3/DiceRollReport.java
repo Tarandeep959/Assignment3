0bb890f9629b59d49a5b292579ec6b7f1317cd97
Lenovo <Lenovo@DESKTOP-2V6JLC8>
2024-04-11 13:53:49 -0400

    ASSIGNEMTN03 CLASS DICROLLREPORT BY TARANDEEP KAUR

---
+++ Assignment03/src/Assignment3/DiceRollReport.java
@@ -0,0 +1,27 @@
+package Assignment3;
+
+public class DiceRollReport {
+	
+	    public static void main(String[] args) {
+	        int roll = 0;
+	        int[] diceRolls = new int[20];
+	        int totalRolls = 0;
+
+	        
+	        for (int count = 0; count < 1000; count++) {
+	            roll = (int) (Math.random() * 20) + 1;
+	            if (roll > 0 && roll <= 20) 
+	                diceRolls[roll - 1]++; 
+	        }
+
+	        
+	        for (int index = 0; index < diceRolls.length; index++) {
+	            System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+	            totalRolls += diceRolls[index]; 
+	        }
+	        System.out.println("Total rolls were: " + totalRolls);
+	        System.out.println("Program by Your Name");
+	    }
+	}
+
+
