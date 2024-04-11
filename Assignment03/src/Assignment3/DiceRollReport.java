a06ebafb2f9653c671a71ecb22028acaaa45b22c
Lenovo <Lenovo@DESKTOP-2V6JLC8>
2024-04-11 13:55:30 -0400

    NEW PROJECT DICEROLLREPORT BY TARANDEEP KAUR

--- Assignment03/src/Assignment3/DiceRollReport.java
+++ Assignment03/src/Assignment3/DiceRollReport.java
@@ -20,7 +20,7 @@ public class DiceRollReport {
 	            totalRolls += diceRolls[index]; 
 	        }
 	        System.out.println("Total rolls were: " + totalRolls);
-	        System.out.println("Program by Your Name");
+	        System.out.println("Program by TARANDEEP KAUR ");
 	    }
 	}
 
